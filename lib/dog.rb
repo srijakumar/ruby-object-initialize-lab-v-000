485c3f98363d88d975ee1d16f7f1af82b368f930
Srija S <srijakumar7@gmail.com>
2017-12-06 02:25:20 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+  def initialize(breed)
+    @breed = "Mutt"
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+end
